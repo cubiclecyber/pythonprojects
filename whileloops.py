7376b3c4ec33ba3bf93e15d66a78492cfb2dc1df
cubicle cyber <cubiclecyber@gmail.com>
2022-03-21 20:32:33 +0300

    Update whileloops.py

--- whileloops.py
+++ whileloops.py
@@ -12,19 +12,19 @@ for j in range (1, 6001):
 # # Write that prompts the user to input student marks. The input should be between 0 and 100.Then output the correct grade: 
 # # A > 79 , B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40
 
-# marks = int(input("Input the marks to get the grade:"))
-# if marks >=79 and marks<=100:
-#     print("A")
-# elif marks >=60 and marks <79:
-#     print("B")
-# elif marks >=49 and marks <=59:
-#     print("C")
-# elif marks >=40 and marks <49:
-#     print("D")
-# elif marks <40:
-#     print("E")
-# else:
-#     print("Incorrect marks")
+marks = int(input("Input the marks to get the grade:"))
+if marks >=79 and marks<=100:
+    print("A")
+elif marks >=60 and marks <79:
+    print("B")
+elif marks >=49 and marks <=59:
+    print("C")
+elif marks >=40 and marks <49:
+    print("D")
+elif marks <40:
+    print("E")
+else:
+    print("Incorrect marks")
 
 # # TASK 9: Using Python or PHP or Java or Ruby or JavaScript
 # # Write a program that takes the age in years and returns the age in days.Use 365 days as the length of a year for this challenge as we would like to ignore leap years. Only accept positive numbers.
@@ -69,23 +69,23 @@ else:
 # # TASK 10: Using Python or PHP or Java or Ruby or JavaScript
 # # Write a program that prompts the user to enter the base and height of a triangle and returns its area.
 # # Once you learn fun 
-# base = float(input("Enter the base of the triangle :"))
-# height = float(input("Enter the height of the triangle :"))
-# area = 1/2 * base * height
-# print ("Area =",area)
+base = float(input("Enter the base of the triangle :"))
+height = float(input("Enter the height of the triangle :"))
+area = 1/2 * base * height
+print ("Area =",area)
 
 # TASK 11: Using Python or PHP or Java or Ruby or JavaScript
 # Write a program that takes as input the speed of a car e.g 80. If the speed is less than 70, it should print "Ok". Otherwise, for every 5 km/s above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points.
 # # For example, if the speed is 80, it should print: "Points: 2". If the driver gets more than 12 points, the function should print: "License suspended".
-# speed = int(input("Enter the speed in Km/s :"))
-# demerit = (speed-70)/5
-# status=""
-# if demerit<=0 :
-#     status ="Okay"
-# elif demerit <12 and demerit>0 :
-#     status ="Points :", str(demerit)
-# elif demerit>=12:
-#     print("Your licence has been terminated!!")
+speed = int(input("Enter the speed in Km/s :"))
+demerit = (speed-70)/5
+status=""
+if demerit<=0 :
+    status ="Okay"
+elif demerit <12 and demerit>0 :
+    status ="Points :", str(demerit)
+elif demerit>=12:
+    print("Your licence has been terminated!!")
 
 
 # Write a program called stars.py. It should prompt the user for a number, and it should print the number of stars till the number entered.
@@ -105,13 +105,13 @@ for i in range(rows):
 # TASK 13: Using Python or PHP or Java or Ruby or JavaScript
 # Write a program that calculates the total stock in a company from the array/list below if we know that the stock is the last digit in every array/list.
 
-# prods = [['omo','30kshs','300'], ['milk','50kshs','200'],['bread','45kshs','359'], ['coffee','5kshs','79']]
+prods = [['omo','30kshs','300'], ['milk','50kshs','200'],['bread','45kshs','359'], ['coffee','5kshs','79']]
 
-# NB: ONCE YOU COPY AND PASTE THE LIST ABOVE,REWRITE THE SINGLE QUOTES AS THE ABOVE LIST WILL GIVE YOU AN ERROR.
+NB: ONCE YOU COPY AND PASTE THE LIST ABOVE,REWRITE THE SINGLE QUOTES AS THE ABOVE LIST WILL GIVE YOU AN ERROR.
 
-# prods = [['omo','30kshs','300'], ['milk','50kshs','200'],['bread','45kshs','359'], ['coffee','5kshs','79']]
-# total_stock = int( prods[0][2]) + int(prods[1][2]) + int(prods[2][2]) + int(prods[3][2])
-# print(total_stock)
+prods = [['omo','30kshs','300'], ['milk','50kshs','200'],['bread','45kshs','359'], ['coffee','5kshs','79']]
+total_stock = int( prods[0][2]) + int(prods[1][2]) + int(prods[2][2]) + int(prods[3][2])
+print(total_stock)
 
 # TASK 14: Using Python or PHP or Java or Ruby or JavaScript
 # Write a program that takes 3 details (name,email and phone number) from a user as input, and stores them in one array/list then validates:
@@ -121,57 +121,57 @@ for i in range(rows):
 # If all these pass, print out the details in one list else display an error message.
 # Once you learn functions,revisit this and write this code inside a function.
 
-# name = input("Enter your name : ")
-# if "1" in name or "2" in name or "3" in name or "4" in name or "5" in name or "6" in name or "7" in name or "8" in name or "9" in name  or "0" in name:
-#     print("Oops..incorrect")
-# else:
-#     print("Looks good.")
-# email = input("Good,now enter your email adress : ")
-# if "@" in email  and "." in email and len(email)>=7:
-#     print("Correct email adress, lets proceed")
-# else:
-#     print("Recheck your email adress")
+name = input("Enter your name : ")
+if "1" in name or "2" in name or "3" in name or "4" in name or "5" in name or "6" in name or "7" in name or "8" in name or "9" in name  or "0" in name:
+    print("Oops..incorrect")
+else:
+    print("Looks good.")
+email = input("Good,now enter your email adress : ")
+if "@" in email  and "." in email and len(email)>=7:
+    print("Correct email adress, lets proceed")
+else:
+    print("Recheck your email adress")
 
-# phone = list(input("Enter your phone number : "))
-# status =""
-# if phone[0] == "0" and phone[1] == "7" and len(phone)==10:
-#     del phone[0]
-#     phone.insert(0, "+"), phone.insert(1, "2"),phone.insert(2, "5"), phone.insert(3,"4")
-#     status = ''.join(phone)
-# elif phone[0] == "0" and phone[1] =="1" and len(phone)==10 :
-#     del phone[0]
-#     phone.insert(0, "+"), phone.insert(1, "2"), phone.insert(2, "5"), phone.insert(3, "4")
-#     status = ''.join(phone)
-# elif phone[0] == "7" and len(phone)==9:
-#     phone.insert(0, "+"), phone.insert(1, "2"), phone.insert(2, "5"), phone.insert(3, "4")
-#     status =''.join(phone)
-# elif phone[0] == "2" and phone[1] =="5" and phone[2] == "4" and len(phone)==12:
-#     phone.insert(0,"+")
-#     status = "".join(phone)
-# elif phone[0] == "+" and phone[1] == "2" and phone[2] == "5" and phone[3] =="4" and len(phone) ==13:
-#     status ="".join(phone)
-# print(name,"you entered the following details")
-# print("Your name : ",name)
-# print("Your email : " ,email)
-# print("Phone number  : ",''.join(status))
+phone = list(input("Enter your phone number : "))
+status =""
+if phone[0] == "0" and phone[1] == "7" and len(phone)==10:
+    del phone[0]
+    phone.insert(0, "+"), phone.insert(1, "2"),phone.insert(2, "5"), phone.insert(3,"4")
+    status = ''.join(phone)
+elif phone[0] == "0" and phone[1] =="1" and len(phone)==10 :
+    del phone[0]
+    phone.insert(0, "+"), phone.insert(1, "2"), phone.insert(2, "5"), phone.insert(3, "4")
+    status = ''.join(phone)
+elif phone[0] == "7" and len(phone)==9:
+    phone.insert(0, "+"), phone.insert(1, "2"), phone.insert(2, "5"), phone.insert(3, "4")
+    status =''.join(phone)
+elif phone[0] == "2" and phone[1] =="5" and phone[2] == "4" and len(phone)==12:
+    phone.insert(0,"+")
+    status = "".join(phone)
+elif phone[0] == "+" and phone[1] == "2" and phone[2] == "5" and phone[3] =="4" and len(phone) ==13:
+    status ="".join(phone)
+print(name,"you entered the following details")
+print("Your name : ",name)
+print("Your email : " ,email)
+print("Phone number  : ",''.join(status))
 
 # TASK 15: Using Python or PHP or Java or Ruby or JavaScript
 # Write a program that prints the largest of 4 inputs taken in from a user.
 #  Assume that the user would not enter any two numbers which are the same
 
-# first = int(input("Enter the first number : "))
-# second = int(input("Enter the second number : "))
-# third =int(input("Enter the third number : "))
-# fourth = int(input("Enter the other number : "))
+first = int(input("Enter the first number : "))
+second = int(input("Enter the second number : "))
+third =int(input("Enter the third number : "))
+fourth = int(input("Enter the other number : "))
 
-# if first>second and first>third and first>fourth :
-#     print(first)
-# elif second>first and second>third and second>fourth :
-#     print(second)
-# elif third>first and third>second and third>fourth :
-#     print(third)
-# elif fourth >first and fourth>second and fourth>third :
-#     print(fourth)
+if first>second and first>third and first>fourth :
+    print(first)
+elif second>first and second>third and second>fourth :
+    print(second)
+elif third>first and third>second and third>fourth :
+    print(third)
+elif fourth >first and fourth>second and fourth>third :
+    print(fourth)
 
 
 # TASK 16: Using Python or PHP or Java or Ruby or JavaScript
@@ -195,16 +195,16 @@ while count<4:
 # Write a program that takes input of 2 values and adds them. The program should only accept numbers and floats only or otherwise display an error “invalid character entered” and take the user to re-enter the inputs .
 # Once you learn functions,revisit this and write this code inside a function.
 
-# num_1 = input("Enter the first number :")
-# num_2 = input("Enter second number : ")
-# if num_1== int : 
-#     sumint = int(num_1)+int(num_2)
-#     print(sumint)
-# elif type(sum)==float:
-#     sumint = float(num_1)+float(num_2)
-#     print(sumint)
-# elif sum ==str:
-#      print("Sting not accepted")
+num_1 = input("Enter the first number :")
+num_2 = input("Enter second number : ")
+if num_1== int : 
+    sumint = int(num_1)+int(num_2)
+    print(sumint)
+elif type(sum)==float:
+    sumint = float(num_1)+float(num_2)
+    print(sumint)
+elif sum ==str:
+     print("Sting not accepted")
 
 
 # TASK 18: Using Python or PHP or Java or Ruby or JavaScript
@@ -224,22 +224,22 @@ while count<4:
 
 # i.e taxable_income = gross salary - NSSF
 
-# Continue with the same program and find the person's PAYEE 
-# quiz 18  # quiz 19  #quiz 20
-# salary = float ( input ( 'Enter your Basic Salary: ' ) )
-# benefits = float ( input ( 'Enter your Benefits: ' ) )
-# gross_salary = (float ( salary ) + int ( benefits ))
-# print ( float ( gross_salary ) )
+Continue with the same program and find the person's PAYEE 
+quiz 18  # quiz 19  #quiz 20
+salary = float ( input ( 'Enter your Basic Salary: ' ) )
+benefits = float ( input ( 'Enter your Benefits: ' ) )
+gross_salary = (float ( salary ) + int ( benefits ))
+print ( float ( gross_salary ) )
 
-# nhif = float ( 5 / 100 ) * gross_salary
-# print ( "nhif :",float ( nhif ) )
-# paye = float ( 10 / 100 ) * gross_salary
-# print ( "payee:",float ( paye ) )
-# nssf = float ( 6 / 100 ) * gross_salary  # 6% of gross salary
-# print ("nssf", float ( nssf ) )
+nhif = float ( 5 / 100 ) * gross_salary
+print ( "nhif :",float ( nhif ) )
+paye = float ( 10 / 100 ) * gross_salary
+print ( "payee:",float ( paye ) )
+nssf = float ( 6 / 100 ) * gross_salary  # 6% of gross salary
+print ("nssf", float ( nssf ) )
 
-# taxable_income = gross_salary - nssf
-# print ("taxaable-income :", float ( taxable_income ) )
+taxable_income = gross_salary - nssf
+print ("taxaable-income :", float ( taxable_income ) )
 
-# net_salary = gross_salary - (nhif + nssf + paye)
-# print ( "Net salary :",net_salary)
+net_salary = gross_salary - (nhif + nssf + paye)
+print ( "Net salary :",net_salary)
